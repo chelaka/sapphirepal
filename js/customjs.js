08d378d4b375aa8a004fde48947a09f189fe0b94
chelaka <chelaka97@gmail.com>
2018-08-30 19:36:49 +0530

    changed homepage css

--- js/customjs.js
+++ js/customjs.js
@@ -5,10 +5,10 @@ $(document).ready(function () {
     var ckbox = $('#navcheck');
     $('input').on('click',function () {
         if (ckbox.is(':checked')) {
-            $(".social-icons ul li").css("color","#ff3f3f");
+            // $(".social-icons ul li").css("color","#ff3f3f");
             $("#home-logo").attr("src","images/assets/main-logo-white.png");
         } else {
-            $(".social-icons ul li").css("color","#3b3b3b");
+            // $(".social-icons ul li").css("color","#3b3b3b");
             $("#home-logo").attr("src","images/assets/main-logo.png");
         }
     });
